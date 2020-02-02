6afe10048ffc667bb8f49970be959a6d762e88cf
lisy0123 <leesese3320@gmail.com>
2020-02-02 13:27:54 +0900

    .

---
+++ everyday_coding/d02/d02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void res(int a)
+{
+    int num = 0;
+    int nums = 0;
+    int s;
+    int sum = 0;
+
+    for (;num < a;)
+    {
+        if (num % 2 == 0)
+            sum += num;
+        if (num == 0)
+            num = 1;
+        else
+        {
+            s = num;
+            num += nums;
+            nums = s;
+        }
+    }
+    printf("Output: %d",sum);
+}
+
+int main()
+{
+    int N;
+
+    printf("Input: N = ");
+    scanf("%d", &N);
+    res(N);
+    
+    return (0);
+}
