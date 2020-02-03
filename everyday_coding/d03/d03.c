1f3672b44d1e9d12dbed47d8310ea31cdc46dfe3
lisy0123 <leesese3320@gmail.com>
2020-02-03 14:51:11 +0900

    .

---
+++ everyday_coding/d03/d03.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void res(int num)
+{
+    int i = 0;
+    if (i == 0)
+        printf("\"");
+    if ()
+    if (i == num)
+        printf("\"");
+}
+
+int main()
+{
+    int a;
+
+    printf("Input: ");
+    scanf("%d", &a);
+    printf("Output: [");
+    res(a);
+    printf("]");
+    return (0);
+}
