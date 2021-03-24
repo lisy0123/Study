da06ab2dc4a4048eea71324995b12cb0f6441432
Sangeun Lee <60209937+lisy0123@users.noreply.github.com>
2021-03-24 14:02:47 +0900

    10845 update

---
+++ baekjoon/bj_10845.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int q[10000];
+int start = 0;
+int end = 0;
+int count = 0;
+ 
+void push(int x)
+{
+    q[end++] = x;
+    ++count;
+}
+
+void pop()
+{
+    if (count != 0) {
+        printf("%d\n", q[start++]);
+        --count;
+    }
+    else
+        printf("-1\n");
+}
+
+void size()
+{
+    printf("%d\n", count);
+}
+
+void empty()
+{
+    if (count == 0)
+        printf("1\n");
+    else
+        printf("0\n");
+}
+
+void front()
+{
+    if (count != 0)
+        printf("%d\n", q[start]);
+    else
+        printf("-1\n");
+}
+
+void back()
+{
+    if (count != 0)
+        printf("%d\n", q[end - 1]);
+    else
+        printf("-1\n");
+}
+
+int main()
+{
+    int n;
+    int i;
+    
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++)
+    {
+        char c[6];
+        int x;
+        scanf("%s", &c);
+        if (!strcmp(c, "push"))
+        {
+            scanf("%d", &x);
+            push(x);
+        }
+        else if (!strcmp(c, "pop"))
+            pop();
+        else if (!strcmp(c, "size"))
+            size();
+        else if (!strcmp(c, "empty"))
+            empty();
+        else if (!strcmp(c, "front"))
+            front();
+        else if (!strcmp(c, "back"))
+            back();
+    }
+    return 0;
+}
