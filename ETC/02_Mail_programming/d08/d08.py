dcfdf60620c3e47d650976535b7235eccffda24c
lisy0123 <leesese3320@gmail.com>
2022-01-28 23:42:37 +0900

    clearup d08,d09,d10

--- ETC/02_Mail_programming/d08/d08.py
+++ ETC/02_Mail_programming/d08/d08.py
@@ -1,15 +1,17 @@
+def solution(nums):
+	nums = list(map(int, nums[1:-1].split(",")))
+	max_num =  max(nums)
+	nums.remove(max_num)
+	lst = []
+	for num in nums:
+		if num != max_num:
+			lst.append(num)
+	try:
+		print ("Output:", max(lst))
+	except:
+		print ("Output: Does not exist.")
+
+
 print ("Input Ex) [10,4,3,-1]")
 nums = input("Input: ")
-
-nums = list(map(int, nums[1:-1].split(",")))
-max_num =  max(nums)
-nums.remove(max_num)
-lst = []
-for num in nums:
-	if num != max_num:
-		lst.append(num)
-
-try:
-	print ("Output:", max(lst))
-except:
-	print ("Output: Does not exist.")
+solution(nums)
--- ETC/02_Mail_programming/d10/d10.py
+++ ETC/02_Mail_programming/d10/d10.py
@@ -1,13 +1,16 @@
+def solution(strings):
+	lst = []
+	st = ""
+	for x in range(0, len(strings)):
+		if strings[x] not in st:
+			st += strings[x]
+		else:
+			lst.append(st)
+			st = strings[x]
+	lst.append(st)
+
+	print(f'Output: {len(max(lst, key=len))} // "{max(lst, key=len)}"')
+
+
 strings = input("Input: ")
-
-lst = []
-st = ""
-for x in range(0, len(strings)):
-	if strings[x] not in st:
-		st += strings[x]
-	else:
-		lst.append(st)
-		st = strings[x]
-lst.append(st)
-
-print(f'Output: {len(max(lst, key=len))} // "{max(lst, key=len)}"')
+solution(strings)
